49d4babe4ea0184ac7263d07731ea90bcfc99a06
stevetodman <stevetodman@Hotmail.com>
2025-05-20 18:24:59 -0500

    Add PWA support

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'cardio-qbank-v1';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './styles.css',
+  './script.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
